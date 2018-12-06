06152e735b2ca1ae99600a4f82ba1cb1e534a230
LionelFeliho <lionel.feliho@grenoble-inp.org>
2018-12-06 20:09:48 +0100

    fichiersSource

---
+++ NativeProject/WindowsFormProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Wrapper;
+
+namespace WindowsFormProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string newPath = string.Concat(Environment.GetEnvironmentVariable("PATH"), ";", Properties.Settings.Default.AdditionalPaths);
+            Environment.SetEnvironmentVariable("PATH", newPath, EnvironmentVariableTarget.Process);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            WrapperClass wc = new WrapperClass();
+            wc.getPriceCallEuro(5000, 3.0, 100, 110, 0.4, 0.01);
+        }
+    }
+}
