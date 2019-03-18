e824ea5d75a0c46ce3473d44c0efb4c71cb08d76
Anass JMARI <anass.jmari@grenoble-inp.org>
2019-03-18 14:38:16 +0100

    kljbzlk

--- include/Tools/Tools.hpp
+++ include/Tools/Tools.hpp
@@ -16,6 +16,8 @@ struct Tools
 
     static const int NumberOfWeeksPerYear = 52;
 
+    static const int NumberOfDaysPerWeek = 5;
+
     static void concatenationMatrice(PnlMat* res, const PnlMat *mat1, const PnlMat *mat2);
 };
 
