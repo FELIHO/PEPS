6801168390e5a87843816666d11c24c8b31d7879
Lionel Feliho <lionel.feliho@hotmail.com>
2019-03-19 10:02:57 +0100

    scriptData

--- DataFeeds/makeData.bat
+++ DataFeeds/makeData.bat
@@ -1,4 +1,7 @@
 python C:\Users\lione\Desktop\Ensimag\PEPS\NEWPEPS\DataFeeds\GetDataFeed.py %*
 "C:\Program Files\R\R-3.4.4\bin\x64\Rscript.exe" GetDataFeed.R
 "C:\Program Files\R\R-3.4.4\bin\x64\Rscript.exe" GetDataExchangeRates.R
+cd C:\Users\lione\Desktop\appliweb\notes-app-api
+serverless invoke local --function createAllData --path C:/Users/lione/Desktop/Ensimag/PEPS/NEWPEPS/DataFeeds/kozei_dataFeed.json
+serverless invoke local --function createAllExchangeRate --path C:/Users/lione/Desktop/Ensimag/PEPS/NEWPEPS/DataFeeds/kozei_dataExchangeRates.json
 pause
