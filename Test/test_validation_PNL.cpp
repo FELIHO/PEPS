1e1c4d960919fc90b9997f2f26bfd53bbf29eb58
Mouhcine Bba <Mouhcine.Bba@ensimag.grenoble-inp.fr>
2019-03-19 14:37:53 +0100

    "errors"

--- Test/test_validation_PNL.cpp
+++ Test/test_validation_PNL.cpp
@@ -1,11 +1,15 @@
 #include "Basket.hpp"
+#include "Asian.hpp"
 #include "MonteCarlo.hpp"
 #include "HedgePortfolio.hpp"
+#include "DataSelecter.hpp"
 #include <stdlib.h>
 
 using namespace std;
 int main(){
-    double T = 1;
+    int nbRebalancementPerStep ;
+    double PL;
+    double T = 8;
     int nbTimeSteps = 1;
     int size = 1;
     double r = 0.04879;
@@ -23,19 +27,57 @@ int main(){
     RandomGen* rng = new PnlRnd(pnlRng);
 
     Basket* call = new Basket(T, nbTimeSteps, size, strike, weights);
+    Asian* opt_asian = new Asian(T,nbTimeSteps,size,strike,weights);
     BlackScholesModel* BMS = new BlackScholesModel(size, r, rho, sigma, spot);
     MonteCarlo* monteCarlo = new MonteCarlo(BMS, call, fdStep, nbSamples);
 
-    PnlMat* Data = pnl_mat_new();
-    BMS->simul_market(Data, T, 365, rng);
+    PnlMat* Dailydata = pnl_mat_new();
+    //Simuler le marché pour huit ans,la matrice Data contiendra les prix des actifs journalières pour 8 ans
+    BMS->asset(Dailydata, T, 8*260, rng);
 
+    DataSelecter *DS = new DataSelecter(Dailydata);
 
-    cout << "test" << endl;
-    HedgePortfolio *hedgePortfolio = new HedgePortfolio(Data, monteCarlo);
-    double PL = hedgePortfolio->HedgeError(Data);
+    PnlMat* Data;
+    HedgePortfolio *hedgePortfolio;
 
+    //Rebalancement Annuel
+    nbRebalancementPerStep = 1;
+    Data = DS->getData(T,nbTimeSteps, nbRebalancementPerStep);
+    cout << "test Rebalancement Annuelle" << endl;
+    cout<<" T : "<<T <<"nbTimeSteps : " << nbTimeSteps<< "nbRebalancementPerStep : "<<nbRebalancementPerStep<<endl;
+    hedgePortfolio = new HedgePortfolio(Data, monteCarlo);
+    PL = hedgePortfolio->HedgeError(Data);
     cout << "P&L = " << PL << endl;
 
+    //Rebalancement mensuel
+    nbRebalancementPerStep = 6;
+    Data = DS->getData(T,nbTimeSteps, nbRebalancementPerStep);
+    cout << "test Rebalancement mensuel" << endl;
+    cout<<" T : "<<T <<"nbTimeSteps : " << nbTimeSteps<< "nbRebalancementPerStep : "<<nbRebalancementPerStep<<endl;
+    hedgePortfolio = new HedgePortfolio(Data, monteCarlo);
+    PL = hedgePortfolio->HedgeError(Data);
+    cout << "P&L = " << PL << endl;
+
+    //Rebalancement hebdomadaire
+    nbRebalancementPerStep = 26;
+    Data = DS->getData(T,nbTimeSteps, nbRebalancementPerStep);
+    cout << "test Rebalancement hebdomadaire" << endl;
+    cout<<" T : "<<T <<"nbTimeSteps : " << nbTimeSteps<< "nbRebalancementPerStep : "<<nbRebalancementPerStep<<endl;
+    hedgePortfolio = new HedgePortfolio(Data, monteCarlo);
+    PL = hedgePortfolio->HedgeError(Data);
+    cout << "P&L = " << PL << endl;
+
+    //Rebalancement journalière
+    nbRebalancementPerStep = 260/2;
+    Data = DS->getData(T,nbTimeSteps, nbRebalancementPerStep);
+    cout << "test Rebalancement journalière" << endl;
+    cout<<" T : "<<T <<"nbTimeSteps : " << nbTimeSteps<< "nbRebalancementPerStep : "<<nbRebalancementPerStep<<endl;
+    hedgePortfolio = new HedgePortfolio(Data, monteCarlo);
+    PL = hedgePortfolio->HedgeError(Data);
+    cout << "P&L = " << PL << endl;
+
+    pnl_mat_free(&Data);
+    pnl_mat_free(&Dailydata);
     delete(BMS);
     delete(monteCarlo);
     delete(hedgePortfolio);
