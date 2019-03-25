450d7fbc2ce4035fcf8296849b28faf3fc9eaa6c
Lionel Feliho <lionel.feliho@hotmail.com>
2019-03-25 16:37:33 +0100

    Computation include

--- include/Computations.hpp
+++ include/Computations.hpp
@@ -1,23 +1,22 @@
 #pragma once
 
 #include <string>
-#include <iostream>
-#include <time.h>
-#include "pnl/pnl_random.h"
-#include "MonteCarlo.hpp"
-#include "Kozei.hpp"
-#include "ParameterEstimation.hpp"
-#include "DataSelecter.hpp"
-#include <list>
+
 
 #define DLLEXP   __declspec( dllexport )
 namespace Computations {
-	/*DLLEXP*/ void calleuro(double &ic, double &prix, int nb_samples, double T,
+	DLLEXP void calleuro(double &ic, double &prix, int nb_samples, double T,
 		double S0, double K, double sigma, double r);
-	/*DLLEXP*/ void getPriceKozeiNative(double &ic, double &prix, int n_samples,
+	DLLEXP void getPriceKozeiNative(double &ic, double &prix, int n_samples,
 		double h, int date, double inv_init, int numberDateEstimation, double r);
-	/*DLLEXP*/ std::string getDeltaKozeiNative(int n_samples,
+	DLLEXP std::string getDeltaKozeiNative(int n_samples,
 		double h, int date, double inv_init, int numberDateEstimation, double r);
-	/*DLLEXP*/ std::string getListPriceKozeiNative(int n_samples,
+	DLLEXP std::string getListPriceKozeiNative(int n_samples,
 		double h, double inv_init, int numberDateEstimation, double r);
+	DLLEXP void getProfitAndLossForward(double &profitAndLoss, int n_samples,
+		double h, double inv_init, double r, int numberOfRebalancement, double rho, double sigma, double spot);
+	DLLEXP void getProfitAndLossBackward(double &profitAndLoss, int n_samples,
+		double h, double inv_init, int numberDateEstimation, double r, int numberOfRebalancement);
+	DLLEXP void puteuro(double &ic, double &prix, int nb_samples, double T,
+		double S0, double K, double sigma, double r);
 }
